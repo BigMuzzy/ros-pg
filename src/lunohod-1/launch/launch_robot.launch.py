e29bb1f734bb24230152b1894e903287c0db4531
SM <spottymarshall@gmail.com>
2025-09-18 05:33:03 +0000

    lidar launch changed to make more stable

--- src/lunohod-1/launch/launch_robot.launch.py
+++ src/lunohod-1/launch/launch_robot.launch.py
@@ -2,53 +2,51 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessStart, OnProcessExit
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='lunohod-1'
 
+    # Robot State Publisher - Start immediately
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    # Include RPLidar launch file
-    rplidar = IncludeLaunchDescription(
+    # Lidar - Start after a short delay to ensure USB is ready
+    rplidar = TimerAction(
+        period=2.0,
+        actions=[
+            IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rplidar.launch.py'
+                    get_package_share_directory(package_name),'launch','rplidar_robust.launch.py'
                 )])
+            )
+        ]
     )
 
-    # Include Camera launch file
-    camera = IncludeLaunchDescription(
+    # Camera - Start after lidar
+    camera = TimerAction(
+        period=4.0,
+        actions=[
+            IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','camera.launch.py'
                 )])
+            )
+        ]
     )
 
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )])
-    # )
-
-
+    # Twist Mux - Start early
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
@@ -57,11 +55,8 @@ def generate_launch_description():
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
 
-    
-
-
+    # Controller Manager - Start after RSP is ready
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
@@ -71,60 +66,41 @@ def generate_launch_description():
                     controller_params_file]
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    # Delay controller manager more to avoid conflicts
+    delayed_controller_manager = TimerAction(period=6.0, actions=[controller_manager])
 
+    # Controllers - Start after controller manager
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner",  # Changed from "spawner.py"
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
-            on_start=[diff_drive_spawner],
+            on_start=[TimerAction(period=3.0, actions=[diff_drive_spawner])],
         )
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner",  # Changed from "spawner.py"
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
-            on_start=[joint_broad_spawner],
+            on_start=[TimerAction(period=2.0, actions=[joint_broad_spawner])],
         )
     )
 
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
+        twist_mux,
         rplidar,
         camera,
-        # joystick,
-        twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
---
+++ src/lunohod-1/launch/rplidar_robust.launch.py
@@ -0,0 +1,85 @@
+import os
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.conditions import IfCondition
+
+def generate_launch_description():
+
+    channel_type = LaunchConfiguration('channel_type', default='serial')
+    lidar_port = LaunchConfiguration('lidar_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='460800')
+    frame_id = LaunchConfiguration('frame_id', default='laser_frame')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Standard')
+    auto_reconnect = LaunchConfiguration('auto_reconnect', default='true')
+
+    return LaunchDescription([
+        
+        DeclareLaunchArgument(
+            'channel_type',
+            default_value='serial'
+        ),
+        DeclareLaunchArgument(
+            'lidar_port',
+            default_value='/dev/ttyUSB0'
+        ),
+        DeclareLaunchArgument(
+            'serial_baudrate',
+            default_value='460800'
+        ),
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value='laser_frame'
+        ),
+        DeclareLaunchArgument(
+            'inverted',
+            default_value='false'
+        ),
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value='true'
+        ),
+        DeclareLaunchArgument(
+            'scan_mode',
+            default_value='Standard'
+        ),
+        DeclareLaunchArgument(
+            'auto_reconnect',
+            default_value='true'
+        ),
+
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            parameters=[{
+                'channel_type': channel_type,
+                'lidar_port': lidar_port, 
+                'serial_baudrate': serial_baudrate, 
+                'frame_id': frame_id,
+                'inverted': inverted, 
+                'angle_compensate': angle_compensate,
+                'scan_mode': scan_mode,
+                'auto_reconnect': auto_reconnect,
+                # Add timeout and retry parameters
+                'timeout': 5.0,
+                'retry_delay': 2.0
+            }],
+            output='screen',
+            respawn=True,  # Automatically restart if node dies
+            respawn_delay=3.0  # Wait 3 seconds before restart
+        ),
+        
+        # Optional: Add a monitoring node
+        Node(
+            package='topic_tools',
+            executable='relay',
+            name='scan_monitor',
+            arguments=['/scan', '/scan_backup'],
+            condition=IfCondition(auto_reconnect),
+            respawn=True
+        )
+    ])
